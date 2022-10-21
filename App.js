c0640b62f37c3a9e71d22236b728ed32b143a1ed
acongkplpe@gmail.com <acongkplpe@gmail.com>
2022-10-21 14:57:59 +0700

    delete app.js

--- App.js
+++ App.js
@@ -1,18 +1,53 @@
-import { SafeAreaView, SnapshotViewIOSComponent, StyleSheet } from "react-native";
-import { View, Text } from 'react-native'
-import styles from "./App.style";
-import React from 'react'
-import Containers from "./src/containers/Containers";
- 
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import TopProduct from './src/components/TopProducts';
+import AllProducts from './src/components/AllProducts';
+import Banner from './src/components/Banner';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Containers/>
-       
- 
-    </SafeAreaView>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.banner}>
+        <Banner />
+      </View>
+      <View style={styles.title}>
+        <Text>Diabetic Diet</Text>
+      </View>
+      <View style={styles.top_product}>
+        <TopProduct />
+      </View>
+      <View style={styles.title_a}>
+        <Text>All Products</Text>
+      </View>
+      <View style={styles.all_product}>
+        <AllProducts/>
+        </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 16,
+  },
+  title: {
+    width: 94,
+    height: 25,
+    left: 24,
+    top: 60,
+  },
+  title_a: {
+    width: 94,
+    height: 25,
+    top: 30,
+  },
+  top_product: {
+    height: 162,
+    top: 45,
+  },
+  all_product:{
+    top: 10,
+  }
+
+});
 
 export default App;
