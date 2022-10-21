e19e009d834e211a1f7ad0c33d2a3db1b39dbe31
acongkplpe@gmail.com <acongkplpe@gmail.com>
2022-10-21 17:03:38 +0700

    fig bug

--- App.js
+++ App.js
@@ -13,11 +13,12 @@ const App = () => {
       <View style={styles.title}>
         <Text>Diabetic Diet</Text>
       </View>
-      <View style={styles.top_product}>
+      <View style={styles.topProduct}>
+        
         <TopProduct />
       </View>
       <View style={styles.title_a}>
-        <Text>All Products</Text>
+       <Text>All Products</Text>
       </View>
       <View style={styles.all_product}>
         <AllProducts/>
@@ -39,10 +40,6 @@ const styles = StyleSheet.create({
     width: 94,
     height: 25,
     top: 30,
-  },
-  top_product: {
-    height: 162,
-    top: 45,
   },
   all_product:{
     top: 10,
